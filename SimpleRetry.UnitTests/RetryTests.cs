a74d3ba2581ee3623b5824287d1d5bd33ebfd160
unknown <JeroenP@PC1504.macaw.nl>
2016-05-04 16:01:56 +0200

    Initial commit

---
+++ SimpleRetry.UnitTests/RetryTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Diagnostics;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace SimpleRetry.UnitTests
+{
+
+    [TestFixture]
+    public class RetryTests
+    {
+
+        private static int _timesCalled;
+
+        [SetUp]
+        public void InitTest()
+        {
+            _timesCalled = 0;
+        }
+
+        [Test]
+        public void should_execute_once_and_then_retry_once()
+        {
+            Retry.Execute(() => AddOne(2), TimeSpan.FromMilliseconds(100), 1);
+            _timesCalled.Should().Be(2);
+        }
+
+        [Test]
+        public void should_throw_exception_when_retry_is_reached()
+        {
+            Action action = () => Retry.Execute(() => AddOne(100), TimeSpan.FromMilliseconds(1), 2);
+            action.ShouldThrow<NotSupportedException>();
+        }
+
+        [Test]
+        public void should_sleep_given_timespan_between_exceptions()
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            Retry.Execute(() => AddOne(4), TimeSpan.FromMilliseconds(500), 3);
+
+            stopwatch.Stop();
+            stopwatch.ElapsedMilliseconds.Should().BeGreaterOrEqualTo(1500);
+            // Do not sleep after last call or before first call.
+            stopwatch.ElapsedMilliseconds.Should().BeLessOrEqualTo(2000);
+        }
+
+        [Test]
+        public void should_throw_exception_when_retry_is_smaller_then_zero()
+        {
+            Action action = () => Retry.Execute(() => AddOne(100), TimeSpan.FromMilliseconds(1), -1);
+            action.ShouldThrow<ArgumentException>();
+        }
+
+        [Test]
+        public void should_only_catch_given_exceptions()
+        {
+            Action action = () => Retry.Execute(() => AddOne(100), TimeSpan.FromMilliseconds(1), 10, null, typeof(DivideByZeroException));
+            action.ShouldThrow<NotSupportedException>();
+        }
+
+        [Test]
+        public void should_catch_child_exception()
+        {
+            Retry.Execute(() => AddOne(2), TimeSpan.FromMilliseconds(100), 1, null, typeof(SystemException));
+            _timesCalled.Should().Be(2);
+        }
+
+
+        public void AddOne(int stopThrowingExceptionAt)
+        {
+            _timesCalled++;
+            if (_timesCalled < stopThrowingExceptionAt)
+            {
+                throw new NotSupportedException();
+            }
+        }
+    }
+}
---
+++ SimpleRetry/Retry.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace SimpleRetry
+{
+    public static class Retry
+    {
+        public static void Execute(Action action, TimeSpan retryInterval, int retryCount, Action executeOnException = null, params Type[] exceptionTypes)
+        {
+            Execute<object>(() =>
+            {
+                action();
+                return null;
+            }, retryInterval, retryCount, executeOnException, exceptionTypes);
+        }
+
+        public static T Execute<T>(Func<T> action, TimeSpan retryInterval, int retryCount, Action executeOnException = null, params Type[] exceptionTypes)
+        {
+            if (retryCount < 0)
+            {
+                throw new ArgumentException($"Retry count cannot be lower then zero. Given value was {retryCount}");
+            }
+
+            var exceptions = new List<Exception>();
+            for (int retry = 0; retry < retryCount + 1; retry++)
+            {
+                try
+                {
+                    return action();
+                }
+                catch (Exception ex)
+                {
+
+                    if (exceptionTypes != null && exceptionTypes.Any() && !exceptionTypes.Any(type => ex.IsOfTypeOrInherits(type)))
+                    {
+                        throw;
+                    }
+
+                    // ReSharper disable once UseNullPropagation
+                    if (executeOnException != null)
+                    {
+                        executeOnException();
+                    }
+
+                    exceptions.Add(ex);
+                    if (retry < retryCount)
+                    {
+                        Thread.Sleep(retryInterval);
+                    }
+                }
+            }
+
+            throw new AggregateException(exceptions);
+        }
+
+        private static bool IsOfTypeOrInherits(this object obj, Type type)
+        {
+            var objectType = obj.GetType();
+
+            while (true)
+            {
+                if (objectType.Equals(type))
+                {
+                    return true;
+                }
+                if ((objectType == objectType.BaseType) || (objectType.BaseType == null))
+                {
+                    return false;
+                }
+                objectType = objectType.BaseType;
+            }
+        }
+    }
+}
