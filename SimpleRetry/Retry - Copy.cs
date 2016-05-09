029729984614f9c97625d75c0f7841ab49cc9d4f
eg20fl <eg20fl@WPU8L0003586>
2016-05-09 16:42:03 +0200

    sync and async retry.

---
+++ SimpleRetry/Retry - Copy.cs
@@ -0,0 +1,123 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Runtime.CompilerServices;
+//using System.Threading;
+//using System.Threading.Tasks;
+
+//namespace SimpleRetry
+//{
+//    public static class CopyRetry
+//    {
+//        public static void Execute(Action action, TimeSpan retryInterval, int retryCount, Action<Exception> executeOnEveryException = null, Action<Exception> executeBeforeFinalException = null, params Type[] exceptionTypesToHandle)
+//        {
+//            Execute<object>(() =>
+//            {
+//                action();
+//                return null;
+//            }, retryInterval, retryCount, executeOnEveryException, executeBeforeFinalException, exceptionTypesToHandle);
+
+
+//        }
+
+//        public static T Execute<T>(Func<T> action, TimeSpan retryInterval, int retryCount, Action<Exception> executeOnEveryException = null, Action<Exception> executeBeforeFinalException = null, params Type[] exceptionTypesToHandle)
+//        {
+//            Task<T> mainTask = new Task<T>(action);
+//            Func<Exception, Task> executeOnEveryExceptionFunc = exception => Task.Run(() =>
+//            {
+//                executeOnEveryException?.Invoke(exception);
+//            });
+
+//            Func<Exception, Task> executeBeforeFinalExceptionFunc = exception => Task.Run(() =>
+//            {
+//                executeBeforeFinalException?.Invoke(exception);
+//            });
+
+//            return ExecuteAsync(() => mainTask, retryInterval, retryCount, executeOnEveryExceptionFunc, executeBeforeFinalExceptionFunc, exceptionTypesToHandle).ConfigureAwait(false).GetAwaiter().GetResult();
+//        }
+
+//        public static async Task ExecuteAsync(Action action, TimeSpan retryInterval, int retryCount, Func<Exception, Task> executeOnEveryException = null, Func<Exception, Task> executeBeforeFinalException = null, params Type[] exceptionTypesToHandle)
+//        {
+//            await ExecuteAsync<object>(() =>
+//            {
+//                action();
+//                return null;
+//            }, retryInterval, retryCount, executeOnEveryException, executeBeforeFinalException, exceptionTypesToHandle);
+//        }
+
+//        public static async Task<T> ExecuteAsync<T>(Func<Task<T>> action, TimeSpan retryInterval, int retryCount, Func<Exception, Task> executeOnEveryException = null, Func<Exception, Task> executeBeforeFinalException = null, params Type[] exceptionTypesToHandle)
+//        {
+//            if (retryCount < 0)
+//            {
+//                throw new ArgumentException($"Retry count cannot be lower then zero. Given value was {retryCount}");
+//            }
+
+//            var exceptions = new List<Exception>();
+//            for (int retry = 0; retry < retryCount + 1; retry++)
+//            {
+//                try
+//                {
+//                    return await action().ConfigureAwait(false);
+//                }
+//                catch (Exception ex)
+//                {
+//                    if (executeOnEveryException != null)
+//                    {
+//                        await executeOnEveryException(ex).ConfigureAwait(false); 
+//                    }
+                    
+
+//                    if (exceptionTypesToHandle != null && exceptionTypesToHandle.Any() && !exceptionTypesToHandle.Any(type => ex.IsOfTypeOrInherits(type)))
+//                    {
+//                        throw;
+//                    }
+
+//                    exceptions.Add(ex);
+//                    if (retry < retryCount)
+//                    {
+//                        Thread.Sleep(retryInterval);
+//                    }
+//                }
+//            }
+
+//            var exceptionToThrow = new AggregateException(exceptions);
+//            if (executeBeforeFinalException != null)
+//            {
+//                await executeBeforeFinalException(exceptionToThrow).ConfigureAwait(false); ;
+//            }
+//            throw exceptionToThrow;
+//        }
+
+//        private static bool IsOfTypeOrInherits(this object obj, Type type)
+//        {
+//            var objectType = obj.GetType();
+
+//            while (true)
+//            {
+//                if (objectType == type)
+//                {
+//                    return true;
+//                }
+//                if ((objectType == objectType.BaseType) || (objectType.BaseType == null))
+//                {
+//                    return false;
+//                }
+//                objectType = objectType.BaseType;
+//            }
+//        }
+
+//        static Task<T> AsAsync<T>(Action<Action<T>> target)
+//        {
+//            var tcs = new TaskCompletionSource<T>();
+//            try
+//            {
+//                target(t => tcs.SetResult(t));
+//            }
+//            catch (Exception ex)
+//            {
+//                tcs.SetException(ex);
+//            }
+//            return tcs.Task;
+//        }
+//    }
+//}
--- SimpleRetry/Retry.cs
+++ SimpleRetry/Retry.cs
@@ -2,21 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace SimpleRetry
 {
     public static class Retry
     {
-        public static void Execute(Action action, TimeSpan retryInterval, int retryCount, Action executeOnException = null, params Type[] exceptionTypes)
+        public static void Execute(Action action, TimeSpan retryInterval, int retryCount, Action<Exception> executeOnEveryException = null, Action<Exception> executeBeforeFinalException = null, params Type[] exceptionTypesToHandle)
         {
             Execute<object>(() =>
             {
                 action();
                 return null;
-            }, retryInterval, retryCount, executeOnException, exceptionTypes);
+            }, retryInterval, retryCount, executeOnEveryException, executeBeforeFinalException, exceptionTypesToHandle);
         }
 
-        public static T Execute<T>(Func<T> action, TimeSpan retryInterval, int retryCount, Action executeOnException = null, params Type[] exceptionTypes)
+
+        public static T Execute<T>(Func<T> action, TimeSpan retryInterval, int retryCount, Action<Exception> executeOnEveryException = null, Action<Exception> executeBeforeFinalException = null, params Type[] exceptionTypesToHandle)
         {
             if (retryCount < 0)
             {
@@ -32,16 +34,11 @@ namespace SimpleRetry
                 }
                 catch (Exception ex)
                 {
+                    executeOnEveryException?.Invoke(ex);
 
-                    if (exceptionTypes != null && exceptionTypes.Any() && !exceptionTypes.Any(type => ex.IsOfTypeOrInherits(type)))
+                    if (exceptionTypesToHandle != null && exceptionTypesToHandle.Any() && !exceptionTypesToHandle.Any(type => ex.IsOfTypeOrInherits(type)))
                     {
                         throw;
-                    }
-
-                    // ReSharper disable once UseNullPropagation
-                    if (executeOnException != null)
-                    {
-                        executeOnException();
                     }
 
                     exceptions.Add(ex);
@@ -52,7 +49,63 @@ namespace SimpleRetry
                 }
             }
 
-            throw new AggregateException(exceptions);
+            var exceptionToThrow = new AggregateException(exceptions);
+            executeBeforeFinalException?.Invoke(exceptionToThrow);
+            throw exceptionToThrow;
+        }
+
+        public static async Task ExecuteAsync(Action action, TimeSpan retryInterval, int retryCount, Func<Exception, Task> executeOnEveryException = null, Func<Exception, Task> executeBeforeFinalException = null, params Type[] exceptionTypesToHandle)
+        {
+            var task = Task<object>.Factory.StartNew(delegate
+            {
+                action();
+                return null;
+            });
+
+            await ExecuteAsync(async () => await task, retryInterval, retryCount, executeOnEveryException, executeBeforeFinalException, exceptionTypesToHandle);
+        }
+
+        public static async Task<T> ExecuteAsync<T>(Func<Task<T>> action, TimeSpan retryInterval, int retryCount, Func<Exception, Task> executeOnEveryException = null, Func<Exception, Task> executeBeforeFinalException = null, params Type[] exceptionTypesToHandle)
+        {
+            if (retryCount < 0)
+            {
+                throw new ArgumentException($"Retry count cannot be lower then zero. Given value was {retryCount}");
+            }
+
+            var exceptions = new List<Exception>();
+            for (int retry = 0; retry < retryCount + 1; retry++)
+            {
+                try
+                {
+                    return await action();
+                }
+                catch (Exception ex)
+                {
+                    if (executeOnEveryException != null)
+                    {
+                        await executeOnEveryException(ex);
+                    }
+                    
+
+                    if (exceptionTypesToHandle != null && exceptionTypesToHandle.Any() && !exceptionTypesToHandle.Any(type => ex.IsOfTypeOrInherits(type)))
+                    {
+                        throw;
+                    }
+
+                    exceptions.Add(ex);
+                    if (retry < retryCount)
+                    {
+                        Thread.Sleep(retryInterval);
+                    }
+                }
+            }
+
+            var exceptionToThrow = new AggregateException(exceptions);
+            if (executeBeforeFinalException != null)
+            {
+                executeBeforeFinalException?.Invoke(exceptionToThrow);
+            }
+            throw exceptionToThrow;
         }
 
         private static bool IsOfTypeOrInherits(this object obj, Type type)
@@ -61,7 +114,7 @@ namespace SimpleRetry
 
             while (true)
             {
-                if (objectType.Equals(type))
+                if (objectType == type)
                 {
                     return true;
                 }
