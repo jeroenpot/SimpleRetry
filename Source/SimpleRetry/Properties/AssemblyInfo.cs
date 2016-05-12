287e5794f01f5fc17c829fae3ed1ecdcedfe7cba
Jeroen Pot <info@jeroenpot.nl>
2016-05-12 09:29:49 +0200

    added nuspec package

--- Source/SimpleRetry/Properties/AssemblyInfo.cs
+++ Source/SimpleRetry/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SimpleRetry")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Retry actions if errors happen. Both sync and async supported")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SimpleRetry")]
