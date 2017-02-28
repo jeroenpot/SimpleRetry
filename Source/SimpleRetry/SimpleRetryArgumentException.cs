0b2816ed5eb33d4288ae6f3a17388cc8c4f87edd
Jeroen Pot <info@jeroenpot.nl>
2017-02-28 13:27:39 +0100

    Support for .NET standards.

--- Source/SimpleRetry/SimpleRetryArgumentException.cs
+++ Source/SimpleRetry/SimpleRetryArgumentException.cs
@@ -1,30 +1,43 @@
 ﻿using System;
-using System.Runtime.Serialization;
 
 namespace SimpleRetry
 {
-    [Serializable]
+    /// <summary>
+    /// Argument Exception used in SimpleRetry library.
+    /// </summary>
+    /// <seealso cref="System.ArgumentException" />
     public class SimpleRetryArgumentException : ArgumentException
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SimpleRetryArgumentException"/> class.
+        /// </summary>
         public SimpleRetryArgumentException()
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SimpleRetryArgumentException"/> class.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
         public SimpleRetryArgumentException(string message) : base(message)
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SimpleRetryArgumentException"/> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="parameter">The parameter.</param>
         public SimpleRetryArgumentException(string message, string parameter) : base(message, parameter)
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SimpleRetryArgumentException"/> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="inner">The inner.</param>
         public SimpleRetryArgumentException(string message, Exception inner) : base(message, inner)
-        {
-        }
-
-        protected SimpleRetryArgumentException(
-            SerializationInfo info,
-            StreamingContext context) : base(info, context)
         {
         }
     }
